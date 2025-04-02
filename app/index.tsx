6c4406536c2602242bbf77f96c689967c3e5cc99
Shqiprim Veseli <shqiprimveseli1122@gmail.com>
2025-04-02 15:27:53 +0200

    initialize the native wind and cionfigured it also posted in expo for testing porpuses

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Pimi 01</Text>
     </View>
   );
 }
